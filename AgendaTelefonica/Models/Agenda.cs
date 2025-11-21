7d4942cb5f351bae2688450ab2caf9f22da6d777
angelofarias <angelo.evang@gmail.com>
2025-11-21 20:59:48 -0300

    do desktop para github

---
+++ AgendaTelefonica/Models/Agenda.cs
@@ -0,0 +1,9 @@
+﻿namespace AgendaTelefonica.Models
+{
+    public class Agenda
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+    }
+}
